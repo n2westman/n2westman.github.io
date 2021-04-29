7925211649427c030862a102bc267e072d53f777
Nickolas Westman <n2westman@gmail.com>
2021-04-29 13:47:01 -0500

    Add initial structure and prettier config

---
+++ seam-carving/src/upload/Uploader.js
@@ -0,0 +1,15 @@
+import "./Uploader.css";
+
+import React from "react";
+
+class Uploader extends React.Component {
+  render() {
+    return (
+      <div className="Uploader" onDragDrop="">
+        Drag + drop your image here.test
+      </div>
+    );
+  }
+}
+
+export default Uploader;

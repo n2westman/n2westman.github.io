59ab600f67c9817f26300815b8a20d6910b71768
Nickolas Westman <n2westman@gmail.com>
2021-05-03 19:07:40 -0500

    Add ability to add a vertical seam

--- seam-carving/src/upload/loadImage.js
+++ seam-carving/src/upload/loadImage.js
@@ -3,6 +3,19 @@ import nj from "numjs";
 
 const size = 400;
 
+function addSeam(img) {
+    const toRet = [];
+    const unrolled = img.tolist();
+    const midpoint = img.shape[1] / 2;
+    const blackPixel = [0, 0, 0, 255];
+    
+    for (let r = 0; r < img.shape[0]; r++) {
+        toRet.push(unrolled[r]);
+        toRet[r].splice(midpoint, 4, blackPixel);
+    }
+    return nj.array(toRet);
+}
+
 function loadImage(src) {
   var $image = new Image();
   $image.crossOrigin = "Anonymous";
@@ -17,16 +30,7 @@ function loadImage(src) {
     }
     var start = new Date().valueOf();
     // process images
-    var img = nj.images.read($image),
-      gray = nj.images.rgb2gray(img),
-      flipped = img.slice(null, [null, null, -1]),
-      scharr = nj.images.scharr(gray), // scharr is a edge detector, like sobel
-      resized = nj.images.resize(img, H / 2, W / 2),
-      zoomed = img.slice(
-        [(img.shape[0] / 4) | 0, ((3 * img.shape[0]) / 4) | 0],
-        [(img.shape[1] / 4) | 0, ((3 * img.shape[1]) / 4) | 0]
-      ),
-      duration = new Date().valueOf() - start;
+    var img = nj.images.read($image);
 
     // display images in canvas
     var $original = document.getElementById("original");
@@ -34,17 +38,16 @@ function loadImage(src) {
     $original.height = H;
     nj.images.save(img, $original);
 
-    const altered = nj.zeros([img.shape[0], img.shape[1] + 1, img.shape[2]]);
-    
-    const first = img.slice([0, img.shape[0]/2]);
-    const second = img.slice([img.shape[0]/2, img.shape[0]]);
-    // const altered = first; //nj.concatenate(first, second).reshape(img.shape);
+    const altered = addSeam(img);
+    console.log(img.shape);
+    console.log(altered.shape);
 
     const $gray = document.getElementById("grayscale");
     $gray.width = W;
     $gray.height = H;
     nj.images.save(altered, $gray);
 
+    const duration = new Date().valueOf() - start;
     document.getElementById("duration").textContent = "" + duration;
     document.getElementById("h").textContent = "" + img.shape[0];
     document.getElementById("w").textContent = "" + img.shape[1];
@@ -52,4 +55,5 @@ function loadImage(src) {
 
   $image.src = src;
 }
+
 export default loadImage;
